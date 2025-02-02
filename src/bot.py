51428fdbc3662e69e436fc6da85a93cd7c323158
VitekBrno <dev@svobodamail.cz>
2025-02-02 10:53:14 +0100

    removed duplicate code

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from .config import DISCORD_TOKEN
-from .commands import SummaryCommands
+from .commands.commands import SummaryCommands
 
 # Initialize bot with intents
 intents = discord.Intents.default()
